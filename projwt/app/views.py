652fffb0ffaf45c62b5639dda6d5857733642651
康鑫 <k506855660@163.com>
2018-07-16 23:47:35 +0800

    add jwt pro

---
+++ projwt/app/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+# Create your views here.
+
+class TestView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response('ok')
